c7e8f1ac385061bc9ce9db16f0a60d43472f1284
Mauro <57550145+mauronaca@users.noreply.github.com>
2021-09-23 14:12:36 +0000

    upgrade legend class

--- src/public/js/events.js
+++ src/public/js/events.js
@@ -3,13 +3,24 @@ import {leyenda_cosecha, leyenda_altura} from "./legends.js";
 
 /* Evento para agregar leyenda */
 /* ---------------------------- */
+
+map.on('overlayadd', (e) => {
+  leyenda_altura.addLegend(map, e);
+  leyenda_cosecha.addLegend(map, e);
+});
+
+map.on('overlayremove', (e) => {
+  leyenda_altura.removeLegend(map, e);
+  leyenda_cosecha.removeLegend(map, e);
+});
+
+/*
 // Para leer: http://132.72.155.230:3838/js/leaflet.html
 let legend = L.control({
   position : "bottomright",
   //pane : 'legend-pane'
 });
 legend.onAdd = function(){
-  /* Si ya esta definido, return nada(?) */
   let div = L.DomUtil.create("div", "legend");
   div.innerHTML = 
   `<img src="https://geoforestal.magyp.gob.ar/geoserver/dpf/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=altura_plantaciones_1">`;
@@ -20,7 +31,7 @@ map.on('overlayadd', (e) => {
   //let target_layer_1 = "elev_2021-03-06_t1100_1629683372509_macizos_concordia";
   let target_layer_2 = "altura_plantaciones_" + "1";
   let target_layer_3 = "altura_plantaciones_" + "2";
-  /* Preguntar si el control ya esta definido! hee */
+
   try{
     let wms_layer_name = e.layer.wmsParams.layers;
     if(wms_layer_name == target_layer_2 || wms_layer_name == target_layer_3){
@@ -54,3 +65,4 @@ map.on('overlayremove', (e) => {
     map.removeControl(legend);
   }
 });
+*/
--- src/public/js/legends.js
+++ src/public/js/legends.js
@@ -22,37 +22,46 @@ L.Control.Legend = L.Control.extend({
   },
 
   addLegend : function(map, e){
-    var layer_name = e.layer.wmsParams.layers;
+    try {
+      var layer_name = e.layer.wmsParams.layers;
 
-    if(this.options.layerName.indexOf(layer_name) != -1){
-      console.log(layer_name);
-      this.addTo(map);
+      if(this.options.layerName.indexOf(layer_name) != -1){
+        this.addTo(map);
+      }
+    } 
+    catch(error) {
+      console.log(error);
     }
   },
 
   removeLegend : function(map, e){
-    let is_target_layer = false;
+    try{
+      let is_target_layer = false;
 
-    map.eachLayer(layer => {
-      if(this.options.layerName.indexOf(layer.wmsParams.layers) != -1){
-        console.log(layer)
-        is_target_layer = true;
+      map.eachLayer(layer => {
+        if(this.options.layerName.indexOf(layer.wmsParams.layers) != -1){
+          is_target_layer = true;
+        }
+      });
+
+      if(!is_target_layer){
+      map.removeControl(this);
       }
-    });
-
-    if(!is_target_layer){
-     map.removeControl(this);
+    }
+    catch(error) {
+      console.log(error);
     }
   }
-
 });
 
 L.control.legend = function(name, opts){
   return new L.Control.Legend(name, opts);
 }; 
 
+/* Creacion de capas */
+
 var leyenda_cosecha = L.control.legend('Leyenda-1', {
-  layerName : ['cosechados_2000_2019_Corrientes', 'cosechados_2000_2019_Misiones'],
+  layerName : ['cosechados_2000_2019_Corrientes', 'cosechados_2000_2019_Misiones', 'cosechados_2000_2019_Entre_Rios'],
   imgURL : "https://geoforestal.magyp.gob.ar/geoserver/dpf/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=cosechados_2000_2019_Corrientes",
   position : "bottomright"
 });
